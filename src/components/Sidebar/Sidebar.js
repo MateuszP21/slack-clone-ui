b58b7ad2a299d8c4ef97f398161055cdf4d3df11
MateuszP21 <jdvozef@gmail.com>
2020-09-23 22:06:07 +0200

    some changes with css

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
 function Sidebar() {
 
     return (
       <div className="sidebar">
-       hehe
+       <Header/>
       </div>
     );
   }
