b54368d8529adb9ad216656ab38f9a42f847dc48
MateuszP21 <jdvozef@gmail.com>
2020-10-01 22:21:22 +0200

    add useMemo

--- src/components/Sidebar/List.js
+++ src/components/Sidebar/List.js
@@ -1,23 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useMemo} from 'react';
 import {IoMdArrowDropright, IoMdArrowDropdown} from 'react-icons/io';
 import ListItem from './ListItem';
 import {componentDidMountLog} from '../../utils/log';
+import {ChatContext} from '../context/chat-context';
+
+function displaySelectChatWhenNoExpand(chat, items) {
+  const selectChat = items.find((item) => item.id === chat?.id);
+  return selectChat ? <ListItem {...{conversation: selectChat, checked: true}}/> : <></>
+}
+
+function displayParticularExpandingIcon(expand) {
+  return expand ? <IoMdArrowDropdown/> : <IoMdArrowDropright/>;
+}
+
+function displayListItems(chat, items, expand) {
+  return expand 
+    ? items.map((item, index) => <ListItem {...{conversation: item, checked: chat?.id === item.id}} key={index}/>) 
+    : displaySelectChatWhenNoExpand(chat, items);
+}
 
 function List({items, title}) {
   const [expand, setExpand] = useState(false);
+  const chat = useContext(ChatContext);
+  const chatAndExpandCondition = !expand && chat;
   componentDidMountLog('LIST');
 
-  return (
+  return useMemo(() => (
     <div className="sidebar__list">
-        <div>
-          <span onClick={() => setExpand(!expand)} className="flex__center sidebar__list--container">
-              <span className="flex__center">{expand ? <IoMdArrowDropdown/> : <IoMdArrowDropright/>}</span>
-              <span className="sidebar__list--title flex__center">{title}</span>
-          </span>
-          {expand ? items.map((item, index) => <ListItem {...{conversation: item}} key={index}/>) : <></>}
-        </div>
+      <div>
+        <span onClick={() => setExpand(!expand)} className="flex__center sidebar__list--container">
+          <span className="flex__center">{displayParticularExpandingIcon(expand)}</span>
+          <span className="sidebar__list--title flex__center">{title}</span>
+        </span>
+        {displayListItems(chat, items, expand)}
+      </div>
     </div>
-  );
+  ), [expand, chatAndExpandCondition, items, title]);
 }
   
 export default List;
--- src/components/Sidebar/ListItem.js
+++ src/components/Sidebar/ListItem.js
@@ -2,13 +2,13 @@ import React, {useContext} from 'react';
 import {DispatchChatContext} from '../context/chat-context';
 import {componentDidMountLog} from '../../utils/log';
 
-function ListItem({conversation}) {
+function ListItem({conversation, checked}) {
     const setChat = useContext(DispatchChatContext);
     componentDidMountLog(`LIST_ITEM ${conversation.name}`);
 
     return (
       <>
-        <input className="sidebar__listitem--input" id={`listitem-${conversation.id}`} type="radio" name="listitem--conversation"/>
+        <input className="sidebar__listitem--input" id={`listitem-${conversation.id}`} type="radio" name="listitem--conversation" defaultChecked={checked}/>
         <label className="sidebar__listitem--label" htmlFor={`listitem-${conversation.id}`} onClick={() => setChat(conversation)}>{conversation.name}</label>
       </>
     );
